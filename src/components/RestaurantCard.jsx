ff6f5d0284e644a36b22a7afe9b9f4586170113c
James Drury <jamesdrury@users.noreply.github.com>
2022-05-22 16:58:58 -0400

    basic UI for delete restaurant

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,4 +1,26 @@
+import * as React from "react";
+import { DeleteOutlined } from '@ant-design/icons';
+
 import * as styles from "./RestaurantCard.module.css";
+
+function DeleteOption({ onClick }) {
+  const [delConfirmationActive, setDelConfirmation] = React.useState(false);
+  const showDelConfirmation = () => setDelConfirmation(true);
+  const hideDelConfirmation = () => setDelConfirmation(false);
+
+  if (delConfirmationActive) {
+    return (
+      <span>
+        <a href="" onClick={hideDelConfirmation}>Cancel</a>
+        <button onClick={onClick}>Confirm Delete</button>
+      </span>
+    );
+  }
+
+  return (
+    <DeleteOutlined onClick={showDelConfirmation} />
+  );
+}
 
 function RestaurantCard({
   name,
@@ -9,6 +31,9 @@ function RestaurantCard({
   neighborhood,
   priceRange,
 }) {
+  function deleteRestaurant() {
+    alert('hi');
+  }
   return (
     <div className={styles.card}>
       <div className={styles.imgContainer}>
@@ -16,6 +41,7 @@ function RestaurantCard({
         <embed src="https://img-placeholder.herokuapp.com/restaurant?w=200&h=200&fit=crop" />
       </div>
       <div className={styles.details}>
+        <DeleteOption onClick={deleteRestaurant} />
         <h2>{name}</h2>
         <p>{averageRating} ⭐️⭐️⭐️★★ ({numOfReviews} reviews)</p>
         <p>
