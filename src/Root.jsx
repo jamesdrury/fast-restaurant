8b6700351fcc1b5d351396a5cd7da18582562686
James Drury <jamesdrury@users.noreply.github.com>
2022-05-22 16:58:58 -0400

    Small refactor to <Root>, move alg setup into a fn

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,28 +1,25 @@
-import algoliasearch from "algoliasearch/lite";
 import { InstantSearch } from "react-instantsearch-hooks";
+import algoliasearch from "algoliasearch/lite";
 
 import { App } from "./App";
 
-const {
-  ALGOLIA_APP_ID,
-  ALGOLIA_API_KEY,
-  ALGOLIA_INDEX_NAME,
-} = process.env;
-
-const searchClient = algoliasearch(
-  ALGOLIA_APP_ID,
-  ALGOLIA_API_KEY,
-);
-
 function Root() {
+  const searchClient = configureAlgoliaSearch();
+  const indexName = process.env.ALGOLIA_INDEX_NAME;
   return (
     <InstantSearch
       searchClient={searchClient}
-      indexName={ALGOLIA_INDEX_NAME}
+      indexName={indexName}
     >
       <App />
     </InstantSearch>
   );
+}
+
+function configureAlgoliaSearch() {
+  const appId = process.env.ALGOLIA_APP_ID;
+  const apiKey = process.env.ALGOLIA_API_KEY;
+  return algoliasearch(appId, apiKey);
 }
 
 export { Root };
