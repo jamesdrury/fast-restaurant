104e39dec8831157b428cd40d593a8e59cce87ec
James Drury <jamesdrury@users.noreply.github.com>
2022-05-22 16:58:58 -0400

    Fix root node appender (must append to body, not document)

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 const node = document.createElement("main");
-document.append(node);
+document.body.append(node);
 const root = createRoot(node);
 root.render(<App />);
