28ab10491735e981511af958d9be5f6520979eb5
James Drury <jamesdrury@users.noreply.github.com>
2022-05-22 16:58:58 -0400

    set up css grid for app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import styles from "./App.module.css";
+
 import { SearchBox } from "./components/SearchBox";
 import { HitContainer } from "./components/HitContainer";
 
 function App() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <header>
         <SearchBox />
       </header>
-      <div className="sidebar">
-        {/* TODO */}
+      <div className={styles.sidebar}>
+        <span>hi</span>
       </div>
-      <main className="results">
+      <main className={styles.results}>
         <HitContainer />
       </main>
     </div>
